b941dea1bd335c63521f56273aa255be3ff86473
0DiegoM <98572984+0DiegoM@users.noreply.github.com>
2022-10-11 18:49:49 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-//https://teachablemachine.withgoogle.com/models/yHfjplUDz/
+//https://teachablemachine.withgoogle.com/models/znK56WAKn/
+
+predicion1 = '';
+
+Webcam.set ({
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById('camera');
+
+Webcam.attach('#camera');
+
+function takeSnapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById('result').innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version', ml5.version);
+
+classifier = ml5.imageClassifer('https://teachablemachine.withgoogle.com/models/znK56WAKn/model.json', modelLoaded);
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speakData1 = 'La prediccion es'+predicion1;
+    var utterThis = new SpeechSynthesisUtterance(speakData1);
+    synth.speak(utterThis);
+}
